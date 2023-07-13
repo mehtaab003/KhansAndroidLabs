8d56f371a6f4bd0063a8dfaa98f375c5b37a6463
khan0498 <khan0498@algonquinlive.com>
2023-07-13 15:27:49 -0400

    Finished Week 7

---
+++ app/src/main/java/algonquin/cst2335/khan0498/data/MessageDatabase.java
@@ -0,0 +1,8 @@
+package algonquin.cst2335.khan0498.data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+@Database(entities = {ChatMessage.class}, version =2)
+public abstract class MessageDatabase extends RoomDatabase {
+    public abstract ChatMessageDAO cmDAO();
+}
